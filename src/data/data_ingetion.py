89e573f3ab29ae1086948f4bb9c9c938b205fef0
unknown <samiabdulsami122010@gmail.com>
2026-01-17 11:20:38 +0500

    work in model and features folder done !

--- src/data/data_ingetion.py
+++ src/data/data_ingetion.py
@@ -71,9 +71,9 @@ def save_data(train_data: pd.DataFrame, test_data: pd.DataFrame, data_path: str)
 
 def main():
     try:
-        # params = load_params(params_path='params.yaml')
-        # test_size = params['data_ingestion']['test_size']
-        test_size = 0.2
+        params = load_params(params_path='params.yaml')
+        test_size = params['data_ingestion']['test_size']
+        # test_size = 0.2
         
         df = load_data(data_url='https://raw.githubusercontent.com/vikashishere/Datasets/refs/heads/main/data.csv')
         # s3 = s3_connection.s3_operations("bucket-name", "accesskey", "secretkey")
