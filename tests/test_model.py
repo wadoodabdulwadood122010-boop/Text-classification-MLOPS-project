3b58326f02a43a9de47c298f00e99ba0f1f3efca
unknown <samiabdulsami122010@gmail.com>
2026-01-19 09:54:39 +0500

    ci 11

--- tests/test_model.py
+++ tests/test_model.py
@@ -14,7 +14,7 @@ class TestModelLoading(unittest.TestCase):
         # Set up DagsHub credentials for MLflow tracking
         dagshub_token = os.getenv("DAGSHUB_KEY")
         if not dagshub_token:
-            raise EnvironmentError("CAPSTONE_TEST environment variable is not set")
+            raise EnvironmentError("DAGSHUB_KEY environment variable is not set")
 
         os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
         os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
